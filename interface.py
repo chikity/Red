73f7ec616d4086e54792f167d46c543f48430b1e
SarthakJShetty <sarthakshetty97@gmail.com>
2020-02-18 15:20:14 +0530

    Interface code to talk to the .csv file and import the data as a pandas dataframe to ensure better modifiability and tweaking.

--- interface.py
+++ interface.py
@@ -5,11 +5,22 @@ Check out the larger project at https://github.com/SarthakJShetty/Red
 -Sarthak
 (18/02/2020)'''
 
-import csv
+'''Reading the csv file as a pandas dataframe and port over the entire column as a list.'''
 import pandas as pd
 
-file = 'data/birds.csv'
+def fileReader(csvFile):
+    '''This function reads the csvFile and sends it over to the scraper'''
+    speciesDF = pd.read_csv(csvFile)
+    return speciesDF
 
-with open(file) as csv_file:
-    for element in csv_file:
-        print(element)
+def dataPorter(speciesDF):
+    '''This function ports the species column of the .csv file to a list and sends it over to the scraper'''
+    listOfSpecies = []
+    '''We use this integer value to keep track of which species is being currently read by the scraper code'''
+    numberOfSpecies = 0
+    '''Looping through the pandas column and appending the listOfSpecies list, this will be returned to the scraper code'''
+    for element in (speciesDF['species']):
+        listOfSpecies.append(element)
+        numberOfSpecies += 1
+    '''Returning the list of species and number of species'''
+    return numberOfSpecies, listOfSpecies

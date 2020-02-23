c47420d3441dcc87c54fe5c6a75fd80f71f831f7
SarthakJShetty <sarthakshetty97@gmail.com>
2020-02-23 13:06:40 +0530

    Comments!

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
+: 'This script is designed to loop through the scraper code over and over again in case the code exits due to a faulty Chrome instance
+or delay in souping the page
+
+-Sarthak
+(23/02/2020)'
+
 while :
     do
         python3 scraper.py
